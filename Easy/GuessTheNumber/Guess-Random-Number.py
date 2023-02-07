9dc09523da9f83068a2b5c29372ab2b41d2b4a62
Ans <anssaeed.pk@gmail.com>
2023-02-07 15:55:09 +0500

    Added Program Rock Paper Scissors

--- Easy/GuessTheNumber/Guess-Random-Number.py
+++ Easy/GuessTheNumber/Guess-Random-Number.py
@@ -1,7 +1,9 @@
-def guess():
-    import random
-    randumnumber=random.randint(1,20)
+#This is a random guess number game
+def guess(): 
+    import random   
+    randumnumber=random.randint(1,20) #Computer will pick one random number between 1 to 20
     n=1
+#Ask User to guess the number 7 times
     print("I am thinking of a number between 1 and 20")
     while n<=7:
         guesstaken=int(input("Take a Guess :"))
@@ -13,12 +15,12 @@ def guess():
             print("Your guess is too high")
             n=n+1
         else:
-            break
+            break   #If the guess is right condition will break
 
     if(guesstaken==randumnumber):
         print(f"Good Job! You guessed my number in {n} guesses")
     else:
         print(f"Nope! The number i was thinking of was {randumnumber}")
 
-
+#Function Call.
 guess()
